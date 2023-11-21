f609e47724aec66c72e1f189285b8c8005db226d
ArCaneSec <arcanebughunter@gmail.com>
2023-11-21 20:01:36 +0330

    celery hotfix

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,22 +1,38 @@
+import logging
+
 from celery import shared_task
 from django.core.mail import send_mail
 
 from .models import Order
 
+logger = logging.getLogger(__name__)
 
-@shared_task()
+
+@shared_task
 def order_created(order_id: int):
     """
-    This task is responsible to send notification via email
-    whenever an order is created.
+    Async task executed by celery worker that send
+    email messages to customer who have created an order.
     """
-    order = Order.objects.get(id=order_id)
-    subject = f"Order nr. {order.id}"
-    message = (
-        f"Dear {order.first_name}\n\n"
-        "you have succesfully placed an order."
-        f"your order id: {order.id}"
-    )
-    mail_sent = send_mail(subject, message, "admin@shop.com", [order.email], fail_silently=False)
-    print("test")
-    return mail_sent
+    try:
+        logger.info(f"Processing order {order_id}")
+        order = Order.objects.get(id=order_id)
+        subject = f"Order nr. {order.id}"
+        message = (
+            f"Dear {order.first_name}\n\n"
+            "you have successfully placed an order."
+            f"your order id: {order.id}"
+        )
+        mail_sent = send_mail(
+            subject,
+            message,
+            "admin@shop.com",
+            [order.email],
+            fail_silently=False,
+        )
+
+        logger.info(f"Mail sent: {mail_sent}")
+        return mail_sent
+    except Exception as e:
+        logger.exception(f"Error processing order {order_id}: {e}")
+        return False
